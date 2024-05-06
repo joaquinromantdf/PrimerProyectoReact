703ba1add0eedf30492e1f20a3fbb66bbedaf6ff
joaquin <joaquinroman2006@gmail.com>
2024-05-06 19:22:10 -0300

    codigo base

---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,15 @@
+import { Contador } from 'react'
+
+export default class App extends Contador{
+
+    render(){
+        return(
+            <div className='Contador'>
+                <span className='Valor'>00</span>
+                <div className='Botonera'>
+        
+                </div>
+            </div>
+        )
+    }
+}
