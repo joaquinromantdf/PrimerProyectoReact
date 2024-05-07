58d7b03da0c6ea4160f6f797594d3f41c42b9c02
joaquin <joaquinroman2006@gmail.com>
2024-05-07 19:24:41 -0300

    acomode las cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Component } from 'react'
-import './App.css'
+import 'App.css'
+import Contador from 'componentes/Contador'
 
 export default class App extends Component{
-
   render(){
     return(
-      <div>OK</div>
+      <div className='ListaContadores'>
+        <Contador />
+        <Contador />
+        <Contador />
+        <Contador />
+        <Contador />
+      </div>
     )
   }
 }
--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,13 +1,29 @@
-import { Contador } from 'react'
+import { Component } from 'react'
 
-export default class App extends Contador{
-
+export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state={valor:0}
+    }
+    restar(){
+        this.stateState({valor: this.state.valor - 1});
+    }
+    sumar(){
+        this.stateState({valor: this.state.valor + 1});
+    }
+    
+    
     render(){
         return(
             <div className='Contador'>
-                <span className='Valor'>00</span>
+                <span className='Valor'>{this.state.valor}</span>
                 <div className='Botonera'>
-        
+                    <Boton simbolo='-'
+                    accion={() => alert("Resta")}
+                    />
+                    <Boton simbolo='+'
+                    accion={() => alert("Suma")}
+                    />
                 </div>
             </div>
         )
